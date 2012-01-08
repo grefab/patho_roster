62b4b2248034f195a97ec36b0bfbedd70dc305f5
vale <vale1410@gmail.com>
2012-01-08 23:18:42 +0100

    always have a result, not respecting min

--- logic/go.sh
+++ logic/go.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+UniqueFolder=tmp
+
+Data=$UniqueFolder/data.tmp
 File=\'$1\'
-Data=test_data.lp
+
 prolog -f j2p.pl -g 'start('$File')' -t 2>>/dev/null halt > $Data
-./run.sh $Data 26 3
+./run.sh $Data 26 5
