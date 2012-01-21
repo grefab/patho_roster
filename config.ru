b48ea06c5e69aca392b77a9f331b3243763d9869
Gregor Fabritius <gre@g0r.de>
2012-01-21 22:07:04 +0100

    renamed "lib" to "rackapp". moved config.ru there. better encapsulation.

--- config.ru
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-require File.expand_path("../lib/patho_roster", __FILE__)
-
-run Sinatra::Application
--- lib/models/employee.rb
+++
@@ -1,9 +0,0 @@
-class Employee
-  def initialize content
-    @name = content[:name]
-    @working = content[:working]
-  end
-
-  attr_accessor :name
-  attr_accessor :working
-end
